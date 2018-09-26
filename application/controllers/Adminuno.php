b3cb1fb1d0562f242b2427ebe2c91026cc09c264
BrandoLarios <brandolarios@outlook.com>
2018-09-26 01:24:10 -0500

    First commit

---
+++ application/controllers/Adminuno.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Adminuno extends CI_Controller {
+
+    
+
+    public function index () {
+        $this->load->view('adminuno/index');
+    }
+
+    
+}
---
+++ application/views/adminuno/index.php
@@ -0,0 +1,25 @@
+<?php 
+    defined('BASEPATH') or exit('No direct script access allowed')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin - Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+    
+</head>
+<body>
+
+    
+    <button type="button" class="btn btn-primary">Primary</button>
+    
+
+    
+    <script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+
+</body>
+</html>
